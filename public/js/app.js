2c2a2a42e96eeccaa87a5472c982ff9d0f4d671b
Ibeh Esther <estheribeh5@gmail.com>
2021-10-11 21:06:43 +0100

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,7 @@ const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data);
-//     })
-// }) 
+
 
 
 weatherForm.addEventListener('submit', (e) => {
@@ -18,7 +14,7 @@ weatherForm.addEventListener('submit', (e) => {
     }
     messageOne.textContent = "Loading ..."; 
     messageTwo.textContent = ''; 
-    fetch('http://localhost:3000/weather?address=' + encodeURIComponent(location)).then((response) => {
+    fetch('/weather?address=' + encodeURIComponent(location)).then((response) => {
     response.json().then((data) => {
         if (data.error) {
            return messageOne.textContent = data.error;
